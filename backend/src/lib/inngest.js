1e86a1afbbaba8fe9aa77be728790bf1a2d79569
rudra2609-06 <rupalthakkar18587@gmail.com>
2026-04-04 21:56:35 +0530

    fixed inngest fn invoking issues

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,21 +1,19 @@
 //A webhook is an automated, HTTP-based callback that allows one application to push real-time data to another whenever a specific event occurs.
 import { Inngest } from "inngest";
-import dbConnect from "./db.js";
 import UserModel from "../models/user.model.js";
 
 export const inngest = new Inngest({ id: "interview-app" });
 
 export const syncUser = inngest.createFunction(
-  { id: "sync-user", trigger: { event: "clerk/user.created" } },
+  { id: "sync-user", triggers: [{ event: "clerk/user.created" }] },
   async ({ event, step }) => {
-    await dbConnect();
-    console.log("DB ready");  
+    console.log("DB ready");
     const { id, image_url, email_addresses, first_name, last_name } =
       event.data;
     const newUser = {
       clerkId: id,
       name: `${first_name || ""} ${last_name}`,
-      email: email_addresses[0].email_address,
+      email: email_addresses[0]?.email_address,
       profileImg: image_url,
     };
     await UserModel.create(newUser);
@@ -24,9 +22,8 @@ export const syncUser = inngest.createFunction(
 );
 
 export const deleteUser = inngest.createFunction(
-  { id: "deleteUser", trigger: { event: "clerk/user.deleted" } },
+  { id: "deleteUser", triggers: [{ event: "clerk/user.deleted" }] },
   async ({ event, step }) => {
-    await dbConnect();
     const { deleted, id } = event.data;
     if (deleted && id) {
       await UserModel.deleteOne({ clerkId: id });
@@ -34,3 +31,5 @@ export const deleteUser = inngest.createFunction(
     // TODO do something else
   },
 );
+
+
