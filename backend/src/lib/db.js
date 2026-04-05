857d0d9b5e39a6babcea3688ecfcbabbd3599b9b
rudra2609-06 <rupalthakkar18587@gmail.com>
2026-04-05 17:11:35 +0530

    stream config addded

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,8 +3,15 @@ import { ENV } from "./env.js";
 
 const dbConnect = async () => {
   try {
+<<<<<<< Updated upstream
     await mongoose.connect(ENV.DB_URI);
     console.log("Db connected");
+=======
+    if (mongoose.connection.readyState >= 1) return;
+
+    await mongoose.connect(ENV.DB_URI);
+    console.log("Db connected successfully");
+>>>>>>> Stashed changes
   } catch (error) {
     console.error("Database connection error:", error.message || error);
     throw error;
--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,13 +1,18 @@
 //A webhook is an automated, HTTP-based callback that allows one application to push real-time data to another whenever a specific event occurs.
 import { Inngest } from "inngest";
 import dbConnect from "./db.js";
+<<<<<<< Updated upstream
 import UserModel from "../models/user.model.js";
+=======
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
+>>>>>>> Stashed changes
 
 export const inngest = new Inngest({ id: "interview-app" });
 
 export const syncUser = inngest.createFunction(
   { id: "sync-user", triggers: { event: "clerk/user.created" } },
   async ({ event, step }) => {
+<<<<<<< Updated upstream
     try {
       await dbConnect();
       const { id, image_url, email_addresses, first_name, last_name } =
@@ -24,13 +29,30 @@ export const syncUser = inngest.createFunction(
       console.error("Error syncing user:", error);
       throw error; // Let Inngest handle the error properly
     }
+=======
+    dbConnect();
+    const { id, image_url, email_addresses, first_name, last_name } =
+      event.data;
+    const newUser = {
+      clerkId: id,
+      name: `${first_name || ""} ${last_name}`,
+      email: email_addresses[0]?.email_address,
+      profileImg: image_url,
+    };
+    await UserModel.create(newUser);
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.profileImg,
+    });
+>>>>>>> Stashed changes
   },
-  // TODO do something else
 );
 
 export const deleteUser = inngest.createFunction(
   { id: "deleteUser", triggers: { event: "clerk/user.deleted" } },
   async ({ event, step }) => {
+<<<<<<< Updated upstream
     try {
       const { deleted, id } = event.data;
       if (deleted && id) {
@@ -40,7 +62,13 @@ export const deleteUser = inngest.createFunction(
     } catch (error) {
       console.error("Error deleting user:", error);
       throw error;
+=======
+    dbConnect();
+    const { deleted, id } = event.data;
+    if (deleted && id) {
+      await UserModel.deleteOne({ clerkId: id });
+      await deleteStreamUser(id.toString());
+>>>>>>> Stashed changes
     }
-    // TODO do something else
   },
 );
