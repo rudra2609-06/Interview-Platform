fdfe05173cba1e77d5b6832740843deaef3d0060
rudra2609-06 <rupalthakkar18587@gmail.com>
2026-04-13 09:33:58 +0530

    fixed url issue

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const apiInstance = axios.create({
-  baseURL: import.meta.env.VITE_DEV_SERVER_URL,
+  baseURL:
+    import.meta.env.VITE_MODE !== "production"
+      ? import.meta.env.VITE_DEV_SERVER_URL
+      : import.meta.env.VITE_BACKEND_URL_WITH_ENDPOINT,
   withCredentials: true,
 });
